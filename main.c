32d59e0c78f8c09434d1383da9a5cb382e4e3dfb
oraziotoscano <oraziotoscano06@gmail.com>
2025-11-10 17:20:08 +0100

    creazione base del lexer

--- main.c
+++ main.c
@@ -1,5 +1,35 @@
 #include <stdio.h>
+#include <string.h>
+
+//LEXER 
+
+enum token_type {
+  ID,
+  ASSIGN_INTVAL,
+  SEMICOLON
+};
+
+
+struct token {
+  enum token_type type;
+};
+
+
 
 int main(){
-  printf("ciao");
+
+  
+  
+  char text[50];
+  
+  fgets(text, 50, stdin);;
+  
+
+  
+  printf("CODE \n");
+  printf("%s", text);
+  
+  
+
+
 }
