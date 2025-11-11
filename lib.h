0a14a36dc5d5bca52b44016befb387001e106592
oraziotoscano <oraziotoscano06@gmail.com>
2025-11-11 02:16:48 +0100

    creazione contatore token

--- lib.h
+++ lib.h
@@ -1,3 +1,32 @@
 #include <stdio.h>
+#include <string.h>
+
+//funzione rimozione newline
+void remove_newline(char text[]){
+  int len = strlen(text);
+  if(len>0 && text[len-1]=='\n'){
+    text[len-1] = '\0';
+  }
+}
 
 
+
+//funzione conta token
+void token_count(char text[], int contatore){
+  unsigned short in_word = 0;   //0 = fuori parola; 1 = dentro la parola
+
+
+    for(int i=0; text[i]!='\0'; i++){
+    if(text[i] != ' '){     //caso in cui siamo in una parola, dunque switchiamo in_word a 1
+      if(in_word == 0){
+        contatore ++;
+        in_word = 1;
+      }
+    }
+      else{                 //caso in cui siamo in uno spazio(fuori parola), e switchiamo in_word a 0
+        in_word = 0;
+      }
+    
+  }
+ printf("numero token: %d", contatore);
+}
--- main.c
+++ main.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <string.h>
+#include "lib.h"
 
 //LEXER 
 
 enum token_type {
   ID,
-  ASSIGN_INTVAL,
-  SEMICOLON
+  ASSIGN,
+  NOT_ASSIGN,
+  SEMICOLON,
+  INTVAL
 };
 
 
@@ -18,18 +21,32 @@ struct token {
 
 int main(){
 
-  
-  
+
+  int num_token = 0;          //variabile contatore token
   char text[50];
   
   fgets(text, 50, stdin);;
-  
+                                            //ci siamo 
 
   
   printf("CODE \n");
   printf("%s", text);
-  
-  
 
 
+
+ //rimuovi newline
+  remove_newline(text);
+
+
+  //controllo se la stringa è vuota
+  if(text[0]=='\0'){
+    printf("token trovati 0 \n");
+    return 0;
+  }
+
+
+  //conta parole (passiamo la stringa e il contatore)
+  token_count(text, num_token);
+
+  return 0;
 }
